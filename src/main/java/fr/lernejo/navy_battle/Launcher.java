98f59576615318fe3b84ad7c70095ae46da4725c
Hamza <hamza.gaizi@epita.fr>
2022-03-30 19:40:34 +0200

    Part2 Final

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -2,6 +2,7 @@ package fr.lernejo.navy_battle;
 
 public class Launcher {
     public static void main(String[] args) {
-
+       MyHttpServer server = new MyHttpServer();
+        server.init();
     }
 }
